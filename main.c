0d6c63aedb4a3dcb8fc62fd051325ecaf77a14c8
anarcopossum <92814289+anarcopossum@users.noreply.github.com>
2022-06-21 19:46:02 -0700

    finished to a threshold of 2

--- main.c
+++ main.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
 
 int main() {
-    printf("Hello, World!\n");
+    // Random Setup
+    time_t t;
+    srand((unsigned) time(&t));
+
+    // Int values we will work with
+    int cor_num = rand() % 100;
+    int guess = 100;
+
+    // prints correct_value
+    printf("%d",cor_num);
+    printf(" solving...\n ");
+
+    /*
+     *guess >= cor_num-2 && guess <= cor_num+2
+     */
+    while(guess != cor_num) {
+        if(guess >= cor_num-2 && guess <= cor_num+2){
+            break;
+        }else{
+            if (guess < cor_num) {
+                guess = guess + (guess / 2);
+            } else if (guess > cor_num) {
+                guess = guess / 2;
+            }
+        }
+    }
+    printf("%d\n",guess);
     return 0;
 }
